1333bba7f8d625b4b37c9ae626b82ad6a72fac98
Tigran <tigransimonyan88@gmail.com>
2019-06-28 16:53:20 +0400

    axios setup

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
 import './index.css';
 import App from './App';
+
+axios.defaults.baseURL = 'https://content.guardianapis.com/';
 
 ReactDOM.render(
     <BrowserRouter>
